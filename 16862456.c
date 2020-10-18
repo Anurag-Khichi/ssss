d39b259c1f5c9bf4ac339a9f94dd3b2397ab2747
yuve3588 <73053339+yuve3588@users.noreply.github.com>
2020-10-18 12:33:07 +0530

    Update 16862456.c

--- 16862456.c
+++ 16862456.c
@@ -4,6 +4,14 @@ int main()
     int i,m,t,s,a,b,j,x,y,z;
     scanf("%d",&t);
     scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
+    scanf("%d",&t);
     while(t--)
     {
         i=0;
